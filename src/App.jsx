2faddacef9dd7a7a0b3ad73838ac8ab8787b608d
Viktor Filippov <jackal3166@gmail.com>
2023-09-24 18:27:01 +0300

    Create List

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import List from './List';
 import { data } from './data';
 import { useState } from 'react';
 
 const App = () => {
     const [people, setPeople] = useState(data);
-    console.log(people);
-    return <h2>Birthday Reminder - Starter</h2>;
+
+    return (
+        <main>
+            <section className='container'>
+                <h3>{people.length} birthdays today</h3>
+                <List people={people}/>
+            </section>
+        </main>
+    );
 };
 
 export default App;
