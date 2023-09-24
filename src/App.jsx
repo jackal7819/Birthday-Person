d5e5fc2a47f7c69ff6fcea6391e7581f98e547ca
Viktor Filippov <jackal3166@gmail.com>
2023-09-24 17:45:39 +0300

    Create CSS

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { data } from './data';
+import { useState } from 'react';
+
+const App = () => {
+    const [people, setPeople] = useState(data);
+    console.log(people);
+    return <h2>Birthday Reminder - Starter</h2>;
+};
+
+export default App;
