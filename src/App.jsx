fb9e7ecf40b522908dcd2923f903143b644cf618
Viktor Filippov <jackal3166@gmail.com>
2023-09-24 18:36:01 +0300

    Create Button Clear

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ import { useState } from 'react';
 const App = () => {
     const [people, setPeople] = useState(data);
 
+    const handleClearAll = () => {
+        setPeople([]);
+    };
+
     return (
         <main>
             <section className='container'>
                 <h3>{people.length} birthdays today</h3>
-                <List people={people}/>
+                <List people={people} />
+                <button onClick={handleClearAll} className='btn btn-block'>
+                    clear all
+                </button>
             </section>
         </main>
     );
